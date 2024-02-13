e1cfda3422c3e5df5d2329bc6dce8c8190f818e6
LuDiemert <lucianadiemert@gmail.com>
2024-02-13 15:48:33 -0300

    feat:add route user vote in a specific poll

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,35 @@
+//user vote in a specific poll
+
+import { z } from "zod";
+import { FastifyInstance } from "fastify";
+import { randomUUID } from "crypto";
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post("/polls/:pollId/votes", async (request, reply) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid(),
+    });
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const { pollId } = voteOnPollParams.parse(request.params);
+    const { pollOptionId } = voteOnPollBody.parse(request.body);
+
+    let { sessionId } = request.cookies
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.setCookie("sessionId", sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true,
+      });
+    }
+
+    return reply.status(201).send( sessionId );
+  });
+}
