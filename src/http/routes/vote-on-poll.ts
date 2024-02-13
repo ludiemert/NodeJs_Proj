db6686821eb1cb8d1ee70e4c98a17cfcbbae1806
LuDiemert <lucianadiemert@gmail.com>
2024-02-13 16:41:57 -0300

    feat:adjustments to single votes

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@
 import { z } from "zod";
 import { FastifyInstance } from "fastify";
 import { randomUUID } from "crypto";
+import { prisma } from "../../lib/prisma";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -17,19 +18,56 @@ export async function voteOnPoll(app: FastifyInstance) {
     const { pollId } = voteOnPollParams.parse(request.params);
     const { pollOptionId } = voteOnPollBody.parse(request.body);
 
-    let { sessionId } = request.cookies
+    let { sessionId } = request.cookies;
+
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId,
+          },
+        },
+      });
+
+      if (
+        userPreviousVoteOnPoll &&
+        userPreviousVoteOnPoll.pollOptionId !== pollOptionId
+      ) {
+        
+        // delete the previous vote - apagar o voto anterior
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id,
+          },
+        });
+      } else if (userPreviousVoteOnPoll) {
+        return reply
+          .status(400)
+          .send({ message: "You already voted on this poll 😉👀" });
+      }
+    }
 
     if (!sessionId) {
       sessionId = randomUUID();
 
       reply.setCookie("sessionId", sessionId, {
-        path: '/',
+        path: "/",
         maxAge: 60 * 60 * 24 * 30, // 30 days
         signed: true,
         httpOnly: true,
       });
     }
 
-    return reply.status(201).send( sessionId );
+    //create a new vote - criar um novo voto
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      },
+    });
+
+    return reply.status(201).send();
   });
 }
