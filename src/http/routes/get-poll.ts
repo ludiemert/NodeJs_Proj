3bee6c093d0b9ee0456f583b1a65f2ea9f3a4c45
LuDiemert <lucianadiemert@gmail.com>
2024-02-14 14:56:31 -0300

    feat: adjustments to the polling route

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -3,6 +3,7 @@
 import { z } from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get("/polls/:pollId", async (request, reply) => {
@@ -26,6 +27,40 @@ export async function getPoll(app: FastifyInstance) {
       },
     });
 
-    return reply.send({ poll });
+    if (!poll) {
+      return reply.status(400).send({ message: "Poll not found!!! 🤷‍♂️🧐" });
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES");
+
+    //console.log(result);
+
+    //config score(number) and ID(string)
+    const votes = result.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1];
+
+        Object.assign(obj, { [line]: Number(score) });
+      }
+
+      return obj;
+    }, {} as Record<string, number>);
+
+    //console.log(votes)
+
+    return reply.send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map((option) => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: option.id in votes ? votes[option.id] : 0,
+          
+          };
+        }),
+      },
+    });
   });
 }
